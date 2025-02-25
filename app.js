a7fdfe6367fa6ce8f1c032a98424d1b35f66ada5
Daw <davidpetrosyan250205@gmail.com>
2025-02-26 00:36:50 +0400

    added new file test.js

---
+++ app.js
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import http from 'http';
+
+const port = 3000;
+const localhost = '127.0.0.1';
+
+const server = http.createServer(async (req, res) => {
+    try {
+        res.statusCode = 200;
+        res.end('hello world!');
+    } catch (err) {
+        res.statusCode = 404;
+        res.end(err.message);
+    }
+});
+
+server.listen(port,localhost,() => {
+    console.log(`Server listening on port ${port}`);
+})
